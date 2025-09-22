a47e868f8dcbb6e0557f3640db15de5349b5205b
omie-d <omkardhomkar322001@gmail.com>
2025-09-22 11:13:50 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 //your JS code here. If required.
+let level = 0;
+let current = target;
+ // traverse upwards until no parent left
+while (current) {
+	level++;
+	current = current.parentElement;
+}
+
+// Show the result
+alert("The level of element is: " + level);
